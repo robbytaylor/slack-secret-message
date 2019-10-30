6756db46393bf605bdbfd259807b162364b5fb3b
Rob Taylor <robbytaylor@users.noreply.github.com>
2019-10-30 17:33:53 +0000

    Update comments based on new approach

--- app/index.js
+++ app/index.js
@@ -8,61 +8,81 @@ const app = new App({
 app.command('/encrypted-message', ({ ack, payload, context }) => {
   ack();
 
-  try {
-    const result = app.client.views.open({
-      token: context.botToken,
-      // Pass a valid trigger_id within 3 seconds of receiving it
-      trigger_id: payload.trigger_id,
-      // View payload
-      view: {
-        type: 'modal',
-        // View identifier
-        callback_id: 'view_1',
-        title: {
-          type: 'plain_text',
-          text: 'Modal title'
-        },
-        blocks: [
-          {
-            type: 'section',
-            text: {
-              type: 'mrkdwn',
-              text: 'Welcome to a modal with _blocks_'
-            },
-            accessory: {
-              type: 'button',
-              text: {
-                type: 'plain_text',
-                text: 'Click me!'
-              },
-              action_id: 'button_abc'
-            }
-          },
-          {
-            type: 'input',
-            block_id: 'input_c',
-            label: {
-              type: 'plain_text',
-              text: 'What are your hopes and dreams?'
-            },
-            element: {
-              type: 'plain_text_input',
-              action_id: 'dreamy_input',
-              multiline: true
-            }
+  console.log(payload, context);
+
+  // Figure out who we're sending the message to.
+  // In a direct message this will be the person being messaged.
+  // For a channel get the users in the channel and show a dropdown to select a user.
+
+  app.client.views.open({
+    token: context.botToken,
+    trigger_id: payload.trigger_id,
+    view: {
+      type: 'modal',
+      callback_id: 'send_message',
+      title: {
+        type: 'plain_text',
+        text: 'Send an encrypted message'
+      },
+      blocks: [
+        {
+          type: 'input',
+          block_id: 'mesage',
+          element: {
+            type: 'plain_text_input',
+            action_id: 'message_input',
+            multiline: true,
+
           }
-        ],
-        submit: {
-          type: 'plain_text',
-          text: 'Submit'
         }
+      ],
+      submit: {
+        type: 'plain_text',
+        text: 'Send'
       }
-    });
-    console.log(result);
-  }
-  catch (error) {
-    console.error(error);
-  }
+    }
+  });
+});
+
+app.action('send_message', ({ ack, payload, context, say }) => {
+  ack();
+
+  console.log(payload, context);
+
+  // Query the Slack API to get an email address for the target user.
+  // Send to sharelock to get a URL for the share.
+  // For now, just send the message.
+
+  // Send a message to the target user telling them that <usernmae> wants to send them an encrypted message
+  say({
+    channel: '', // Target user ID
+    blocks: [{
+      "type": "section",
+      "text": {
+        "type": "plain_text",
+        "text": `${payload.user} has sent you an encrypted message`
+      },
+      "accessory": {
+        "type": "button",
+        "text": {
+          "type": "plain_text",
+          "text": "View message"
+        },
+        "action_id": "view_message"
+      }
+    }]
+  });
+});
+
+app.action('view_message', ({ ack, payload, context }) => {
+  ack();
+
+  app.client.views.open({
+    token: context.botToken,
+    trigger_id: payload.trigger_id,
+    view: {
+    }
+  });
 });
 
 (async () => {
